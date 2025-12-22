5c9c099c2ada7aea14320b009b8f00ed3defe5f5
Will Corvo <nick.neytron@yandex.ru>
2025-12-22 14:08:01 +0300

    removed elf_resolve_symbol

--- components/elf_loader/src/elf_symbols.c
+++ components/elf_loader/src/elf_symbols.c
@@ -68,48 +68,6 @@ void* elf_lookup_export(const char* name) {
 	return NULL;
 }
 
-uint32_t elf_resolve_symbol(elf_context_t* ctx, uint32_t sym_idx) {
-	if (!ctx || !ctx->symtab || sym_idx >= ctx->symtab_count) {
-		return 0;
-	}
-
-	const Elf32_Sym* sym = &ctx->symtab[sym_idx];
-	const char* name = ctx->strtab ? (ctx->strtab + sym->st_name) : "";
-	uint16_t shndx = sym->st_shndx;
-
-	if (ctx->debug >= 2) {
-		printf("[sym] Resolving [%lu] '%s': shndx=%u, value=0x%lx\n", (unsigned long)sym_idx, name, shndx, (unsigned long)sym->st_value);
-	}
-
-	if (shndx == SHN_UNDEF) {
-		void* addr = elf_lookup_export(name);
-		if (addr) {
-			if (ctx->debug >= 1) {
-				printf("[sym] External '%s' -> %p\n", name, addr);
-			}
-			return (uint32_t)addr;
-		}
-		printf("[sym] ERROR: Unresolved '%s'\n", name);
-		return 0;
-	}
-
-	if (shndx == SHN_ABS) {
-		return sym->st_value;
-	}
-
-	if (shndx < ctx->section_count && ctx->section_addrs[shndx]) {
-		uint32_t addr = (uint32_t)ctx->section_addrs[shndx] + sym->st_value;
-		if (ctx->debug >= 2) {
-			printf("[sym] '%s' -> sec[%u] + 0x%lx = %p\n", name, shndx, (unsigned long)sym->st_value, (void*)addr);
-		}
-		return addr;
-	}
-
-	printf("[sym] ERROR: Section %u not loaded for '%s'\n", shndx, name);
-
-	return 0;
-}
-
 static void guest_delay_ms(uint32_t ms) {
 	vTaskDelay(pdMS_TO_TICKS(ms));
 }
