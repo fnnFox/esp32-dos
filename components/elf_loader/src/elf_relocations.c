6a58db0c2518153b43623f8a4487f75bdc230038
Will Corvo <nick.neytron@yandex.ru>
2025-12-30 03:01:40 +0300

    rewrite elf_apply_relocations

--- components/elf_loader/src/elf_relocations.c
+++ components/elf_loader/src/elf_relocations.c
@@ -36,8 +36,6 @@ int elf_apply_relocations(elf_context_t* ctx) {
 			printf("[rel] Section '%s' -> target [%lu]\n", name, (unsigned long)target_idx);
 		}
 
-		uint32_t target_base = ctx->shdrs[target_idx].sh_addr;
-
 		int rela_count = shdr->sh_size / sizeof(Elf32_Rela);
 
 		const Elf32_Rela* relas = (Elf32_Rela*)(ctx->elf_data + shdr->sh_offset);
@@ -48,7 +46,7 @@ int elf_apply_relocations(elf_context_t* ctx) {
 			int type = rela->r_info & 0xFF;
 			int idx = rela->r_info >> 8;
 
-			uint32_t target_address = target_base + rela->r_offset;
+			uint8_t* patch_ptr = (uint8_t*)ctx->elf_data + ctx->shdrs[target_idx].sh_offset + rela->r_offset;
 
 			uint32_t symbol_address = elf_resolve_symbol(ctx, idx);
 
@@ -61,18 +59,29 @@ int elf_apply_relocations(elf_context_t* ctx) {
 
 			switch (type) {
 				case R_XTENSA_32: {
-					int is_iram = (target_address >= (uint32_t)ctx->iram_block && target_address < (uint32_t)ctx->iram_block + ctx->iram_size);
-					elf_write32((void*)target_address, value, is_iram);
+					elf_write32((void*)patch_ptr, value);
 					
 					if (ctx->debug >= 2) {
-						printf("[rel] R_XTENSA_32: [0x%08lx] = 0x%08lx\n", target_address, value);
+						printf("[rel] R_XTENSA_32: [0x%08lx] = 0x%08lx\n", *(uint32_t*)patch_ptr, value);
 					}
 					break;
 				}
 				
 				case R_XTENSA_SLOT0_OP: {
+					uint32_t inst = elf_read24((void*)patch_ptr);
+					printf("[op0] Reading 3 bytes from 0x%08lx: 0x%06lx\n", *(uint32_t*)patch_ptr, inst);
+					int op0 = inst & 0x0F;
+
+					if (op0 == 0x01) {
+						uint32_t pc_aligned = (ctx->shdrs[target_idx].sh_addr + rela->r_offset + 3) & ~3;
+						int32_t offset_bytes = (int32_t)(value - pc_aligned);
+						int32_t offset_words = offset_bytes >> 2;
+						inst = (inst & 0xFF) | ((offset_words & 0xFFFF) << 8);
+						elf_write24((void*)patch_ptr, inst);
+					}
+
 					if (ctx->debug >= 2) {
-						printf("[rel] R_XTENSA_SLOT0_OP: offset=0x%lx (skipped)\n", rela->r_offset);
+						printf("[rel] R_XTENSA_SLOT0_OP: [0x%08lx] = 0x%06lx\n", *(uint32_t*)patch_ptr, inst);
 					}
 					break;
 				}
